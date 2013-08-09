f3bdef7270fc17b19dc9c7771a58cc7d45898110
Jeremy Lightsmith <jeremy.lightsmith@gmail.com>
2013-08-09 12:09:14 -0700

    first couple projects

--- public/past_projects.php
+++ public/past_projects.php
@@ -1,5 +1,32 @@
 <?php $title = "Projects"; ?>
 <?php include ( "_header.php" ); ?>
 
+<div class="container">
+  <div class="row">
+    <div class="col-lg-3">
+      <div class="thumbnail">
+        <img src="/images/projects/decks.jpg" alt="">
+        <div class="caption">
+          <h3>Decks</h3>
+          <p>Decks are fun!</p>
+          <p><a href="/projects/deck/" data-target="#simple-modal" data-toggle="modal" class="btn btn-primary">Read more...</a></p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-lg-3">
+      <div class="thumbnail">
+        <img src="/images/projects/shower.jpg" alt="">
+        <div class="caption">
+          <h3>Shower</h3>
+          <p>Showers are fun!</p>
+          <p><a href="/projects/shower_job/" data-target="#simple-modal" data-toggle="modal" class="btn btn-primary">Read more...</a></p>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</div>
+
 
 <?php include ( "_footer.php" ); ?>
