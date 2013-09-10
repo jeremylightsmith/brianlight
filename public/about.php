31352b0cee1f3211e0d952e76b6e77df8911c1c1
Jeremy Lightsmith <jeremy.lightsmith@gmail.com>
2013-09-10 12:51:55 -0700

    adding an about page fixing menu because of new link for different sizes adding pikachoose

--- public/about.php
+++ public/about.php
@@ -1,10 +1,35 @@
-<?php $title = "About"; ?>
+<?php $title = "About Brian"; ?>
 <?php include ( "_header.php" ); ?>
 
 <div class="container">
   <div class="row">
-    <div class="col-12">
-
+    <div class="col-lg-8 col-sm-8">
+      <p>
+        My name is Brian Lee Light. Lee is after my dad Leland who when I was young started selling off parts of his Ohio farm land to the neighbors I grew up with and then would volunteer the next few months helping them build their home. I spent my boyhood in his cabinet shop where my love for building started. I also inherited his desire to help see other's dreams fulfilled the best I can.
+      </p>
+      <p>
+        An education for building and especially remodeling is learned best by hands on efforts. I started with 3 years at a property management company seeing what all can need fixed on a building. I moved on to my own general contractors license in 1997 and have since mixed in time as a certified home inspector, a subcontractor for other contractors (trim work, tile, painting, etc.), a tile installer for several flooring companies, the "go to" man for various property management companies for their larger projects, and with several interior decorators to bring their work to life. I have also taught community classes on "How to finish your basement".
+      </p>
+      <p>
+        The remodeling industry requires a broad knowledge of the trades and experience that has seen vast scenarios. The remodeling industry needs people who are reliable, timely, honest, and qualified. I am striving to bring the remodeling industry someone who is all these things but who is also kind and caring for your desires and what you want to see accomplished. To be someone who fulfills my needs in providing for my family but does it while giving my community a sought after quality at fair market price.
+      </p>
+      <p>
+        I believe most people work hard to earn their money. When it's decided to spend it on enriching the space they live in I want to ensure that it's done buy someone who cares for the details and the emotions of the goal not just money. When we work together to bring your project to fruition it is done directly by me. I am the craftsman. You'll find my timelines accurate. My clients call for my services over and over through the years. I do care.
+      </p>
+      <p>
+        I hope your remodeling experience is a pleasant one. I hope you're listened to. I hope you're treated fairly. I hope your dream comes true.  I hope you will allow me to help you.
+      </p>
+      <p>
+        <img src="/images/signature.gif"></img>
+      </p>
+    </div>
+    <div class="col-lg-4 col-sm-4">
+      <img src="/images/about1.jpg" class="img-responsive"/>
+      <h3>Brian Light</h3>
+      <p>General Contractor, Certified + Bonded</p>
+      <p>
+        <a href="/get_a_bid" class="btn btn-primary">Call me today!</a>
+      </p>
     </div>
   </div>
 </div>
